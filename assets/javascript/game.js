e8ac91681cfea12a006aad4821331fd6b5654120
lschmittling <lanceschmittling@Lances-MBP>
2018-02-01 22:40:30 -0600

    Minor changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,12 +35,47 @@ var losses = 0;
 // Stores the underscores to replace the random word selected
 var solution = [];
 
+var userGuess = [];
+
 // Selects random word from array
 var word = wordList[Math.floor(Math.random() * wordList.length)];
 
-// Replaces word with underscores
-for (var i = 0; i < word.length; i++) {
-    solution[i] = "_";
-}
+// // Replaces word with underscores
+// for (var i = 0; i < word.length; i++) {
+//     solution[i] = "_";
+
+    // On Key Up function
+    document.onkeyup = function (event) {
+        var solution = String.fromCharCode(event.keyCode).toLowerCase();
+
+        if (lettersGuessed.indexOf(userGuess) < 0 && word.indexOf(userGuess) >= 0) {
+            lettersGuessed[lettersGuessed.length] = userGuess;
+            maxTries--;
+        }
+
+        if (word == userGuess) {
+            wins++;
+            guessesLeft = 10;
+            guessesSoFar = [];
+            word = wordList[Math.floor(Math.random() * wordList.length)];
+        }
+
+        if (maxTries == 0) {
+            losses++;
+            maxTries = 10;
+            lettersGuessed = [];
+            word = wordList[Math.floor(Math.random() * wordList.length)];
+        }
+
+        var html =
+            "<p>You chose: " + userGuess + "</p>" +
+            "<p>Remaining Guesses: " + maxTries + "</p>" +
+            "<p>Wins: " + wins + "</p>" +
+            "<p>Losses: " + losses + "</p>";
+
+        // Set the inner HTML contents of the #game div to our html string
+        document.querySelector("#game").innerHTML = html;
+    };
+
 
 console.log(solution);
